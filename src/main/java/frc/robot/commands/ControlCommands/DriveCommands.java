bcf3de82df71ec5674537513e782768b6b0a4a77
Mz <mzmud@icloud.com>
2024-02-25 15:24:08 -0600

    tentative autos

--- src/main/java/frc/robot/commands/ControlCommands/DriveCommands.java
+++ src/main/java/frc/robot/commands/ControlCommands/DriveCommands.java
@@ -39,7 +39,6 @@ public class DriveCommands {
       DoubleSupplier omegaSupplier) {
     return Commands.run(
         () -> {
-        
           double linearMagnitude;
           Rotation2d linearDirection;
           double omega;
@@ -57,15 +56,16 @@ public class DriveCommands {
             omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);
           } else {
             linearMagnitude =
-            (MathUtil.applyDeadband(
-                Math.hypot(xSupplier.getAsDouble(), ySupplier.getAsDouble()), DEADBAND)) / 2;
+                (MathUtil.applyDeadband(
+                        Math.hypot(xSupplier.getAsDouble(), ySupplier.getAsDouble()), DEADBAND))
+                    / 2;
             linearDirection =
                 new Rotation2d(
                     -MathUtil.applyDeadband(xSupplier.getAsDouble(), DEADBAND),
                     -MathUtil.applyDeadband(ySupplier.getAsDouble(), DEADBAND));
             omega = (MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND)) / 2;
           }
-          
+
           // Square values
           linearMagnitude = linearMagnitude * linearMagnitude;
           omega = -Math.copySign(omega * omega, omega);
