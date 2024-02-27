faeacc8e81165712f3874df48eb5071c01f68296
24cirinom <112493487+24cirinom@users.noreply.github.com>
2024-02-27 17:31:52 -0600

    negative gyro

--- src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
@@ -39,7 +39,7 @@ public class GyroIONavX implements GyroIO {
     ahrs.zeroYaw();
 
     yawPositionQueue =
-        SparkMaxOdometryThread.getInstance().registerSignal(() -> (double) ahrs.getYaw() * -1);
+        SparkMaxOdometryThread.getInstance().registerSignal(() -> (double) -ahrs.getYaw());
   }
 
   @Override
@@ -55,19 +55,19 @@ public class GyroIONavX implements GyroIO {
     // yawPositionQueue.clear();
     inputs.connected = ahrs.isConnected();
     inputs.calibrating = ahrs.isCalibrating();
-    inputs.yawPosition = Rotation2d.fromDegrees(ahrs.getYaw() * -1);
+    inputs.yawPosition = Rotation2d.fromDegrees(-ahrs.getYaw());
     inputs.yawVelocityRadPerSec = Units.degreesToRadians(-ahrs.getRawGyroZ());
     inputs.odometryYawPositions =
         yawPositionQueue.stream()
-            .map((Double value) -> Rotation2d.fromDegrees(-value))
+            .map((Double value) -> Rotation2d.fromDegrees(value))
             .toArray(Rotation2d[]::new);
 
     yawPositionQueue.clear();
   }
 
   public void resetRotation() {
-    ahrs.zeroYaw();
-    // ahrs.setAngleAdjustment(ahrs.getAngleAdjustment());
+    // ahrs.zeroYaw();
+    ahrs.setAngleAdjustment(ahrs.getYaw());
   }
 
   @AutoLogOutput(key = "Test/NavXAngleAdjustment")
