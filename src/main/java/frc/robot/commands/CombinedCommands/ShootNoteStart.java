1ef427db95620b22af40a00b9e0f852947ccc1d4
Mz <mzmud@icloud.com>
2024-02-29 21:47:50 -0600

    a TON of autos and combined commands

--- src/main/java/frc/robot/commands/CombinedCommands/ShootNoteStart.java
+++ src/main/java/frc/robot/commands/CombinedCommands/ShootNoteStart.java
@@ -2,46 +2,32 @@ package frc.robot.commands.CombinedCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.commands.ControlCommands.DriveCommands;
 import frc.robot.commands.ControlCommands.IntakeShooterControls;
-import frc.robot.commands.Positions.FloorPickup;
 import frc.robot.commands.Positions.ShootClosePosition;
 import frc.robot.commands.Positions.StowPosition;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.armElevator.ArmElevator;
-import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.flywheel.Flywheel;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.wrist.Wrist;
 
-public class ShootNoteClose {
+public class ShootNoteStart {
 
-  public ShootNoteClose() {}
+  public ShootNoteStart() {}
 
   public static Command run(
-      Intake sIntake,
-      Arm sArm,
-      ArmElevator sArmElevator,
-      Wrist sWrist,
-      Flywheel sFlywheel,
-      Drive sDrive) {
+      Intake sIntake, Arm sArm, ArmElevator sArmElevator, Wrist sWrist, Flywheel sFlywheel) {
     return Commands.run(
         () -> {
-          FloorPickup.run(sArm, sArmElevator, sWrist);
-          DriveCommands.runOverClosestNote(
-              sDrive, sIntake, sFlywheel); // includes intake of note theoretically
-
           ShootClosePosition.run(sArm, sArmElevator, sWrist);
           IntakeShooterControls.intakeShooterDrive(sIntake, sFlywheel, () -> 0, () -> 1, () -> true)
               .withTimeout(3);
-
           StowPosition.run(sArm, sArmElevator, sWrist);
         },
         sIntake,
         sArm,
         sArmElevator,
         sWrist,
-        sFlywheel,
-        sDrive);
+        sFlywheel);
   }
 }
