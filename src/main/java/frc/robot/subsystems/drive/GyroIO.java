709d540651242b357461de3e4118d9df310f8416
MZ <63869775+JadedHearth@users.noreply.github.com>
2024-03-29 14:54:26 -0500

    Add error checking for high frequency odometry Spark Max values (upstream)

--- src/main/java/frc/robot/subsystems/drive/GyroIO.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIO.java
@@ -20,7 +20,7 @@ public interface GyroIO {
   @AutoLog
   public static class GyroIOInputs {
     public boolean connected = false;
-    public boolean calibrating = false;
+    public boolean notcalibrating = false;
     public Rotation2d yawPosition = new Rotation2d();
     public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
     public double[] odometryYawTimestamps = new double[] {};
